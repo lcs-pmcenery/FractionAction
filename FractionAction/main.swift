ee8f54b2be0a4add62527009e6a3d3b273f2d5ed
McEnery Peter <pmcenery@lcs-mba3012.lcs.on.ca>
2018-05-04 12:55:57 -0400

    Got input filtering working for getting a numerator

--- FractionAction/main.swift
+++ FractionAction/main.swift
@@ -1,6 +1,6 @@
 //
 //  main.swift
-//  ChipsFastFoodEmporium
+//  FractionAction
 //
 //  Created by Gordon, Russell on 2018-04-04.
 //  Copyright © 2018 Gordon, Russell. All rights reserved.
@@ -12,6 +12,45 @@ import Foundation
 // Collect and filter user input here
 
 
+var numerator = -1 //outside the loop
+prompt: while 1 == 1 {
+    
+    //Prompt the user
+    print("Numerator?")
+    
+    //wait for input
+    //make sure it is not nil
+    guard let givenInput = readLine() else {
+        
+        //if we got here, input is nil
+        //prompt again
+        continue  //continue with the next interation of 'while' loop
+        
+    }
+    
+    // Test #2: is the input the correct type
+    guard let givenInteger = Int(givenInput) else {
+        
+        //if we got here, input is not a number
+        continue //prompt again
+        
+    }
+    
+    // test #3: Make sure numerator can't be negative
+    if givenInteger < 0 {
+        
+        // aw, bad input, numerator can't be negative
+        continue
+        
+    }
+    
+    // if we got to this point, we know the input is good
+    numerator = givenInteger
+    break   //quit the while loop and begin the PROCESS section
+    
+}
+
+
 // PROCESS
 // Implement the primary logic of the problem here
 
@@ -19,3 +58,4 @@ import Foundation
 // OUTPUT
 // Report results to the user here
 
+let quoitent = numerator / denominator
